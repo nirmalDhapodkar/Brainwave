14585464a8af4bc8185221c636f8873e1def9ed5
stormValet <nirmaldh759@gmail.com>
2024-09-03 23:03:27 +0530

    Improved Hero Section
    Added parallax effect to look like more modern UI

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import Section from "./Section";
 import Button from "./Button";
-import { curve, heroBackground } from "../assets";
-import { robot } from "../assets";
+import { curve, heroBackground, robot } from "../assets";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
+
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
@@ -12,10 +17,10 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-      <div className="container relative">
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of AI Chatting with Brainwave
+            Explore the Possibilities of AI Chatting with{" "}
             <span className="inline-block relative">
               Brainwave{" "}
               <img
@@ -51,8 +56,20 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border boreder-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} widht={24} height={25} alt={icon} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
               </div>
             </div>
+
+            <Gradient />
           </div>
           <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
@@ -63,6 +80,8 @@ const Hero = () => {
               alt="hero"
             />
           </div>
+
+          <BackgroundCircles />
         </div>
       </div>
     </Section>
